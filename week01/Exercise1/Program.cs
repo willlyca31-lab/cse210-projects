2d61f3bc04fb8fd4d91fd12fd0c168e28f169140
willlyca31-lab <willlyca31@gmail.com>
2026-05-09 03:21:33 -0600

    added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Yur name is {last}, {first} {last}");
+
     }
 }
