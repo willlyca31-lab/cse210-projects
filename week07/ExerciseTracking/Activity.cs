de7d8efd6a91116af67edda684959c8f97aabfda
willlyca31-lab <willlyca31@gmail.com>
2026-06-16 20:39:05 -0600

    add activity

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,33 @@
+using System;
+
+public abstract class Activity
+{
+private string _date;
+private int _minutes;
+
+public Activity(string date, int minutes)
+{
+    _date = date;
+    _minutes = minutes;
+}
+
+public string GetDate()
+{
+    return _date;
+}
+
+public int GetMinutes()
+{
+    return _minutes;
+}
+
+public abstract double GetDistance();
+public abstract double GetSpeed();
+public abstract double GetPace();
+
+public virtual string GetSummary()
+{
+    return $"{_date} {GetType().Name} ({_minutes} min): Distance {GetDistance():F1} miles, Speed {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
+}
+
+}
