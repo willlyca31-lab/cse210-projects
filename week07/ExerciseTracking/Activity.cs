b916474b84f5242ae5c30c22174630920a388564
willlyca31-lab <willlyca31@gmail.com>
2026-06-16 20:48:39 -0600

    add ac

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -2,32 +2,35 @@ using System;
 
 public abstract class Activity
 {
-private string _date;
-private int _minutes;
+    private string _date;
+    private int _minutes;
 
-public Activity(string date, int minutes)
-{
-    _date = date;
-    _minutes = minutes;
-}
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
 
-public string GetDate()
-{
-    return _date;
-}
+    public string GetDate()
+    {
+        return _date;
+    }
 
-public int GetMinutes()
-{
-    return _minutes;
-}
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
 
-public abstract double GetDistance();
-public abstract double GetSpeed();
-public abstract double GetPace();
+    // Abstract methods to be overridden by derived classes
+    public abstract double GetDistance();
 
-public virtual string GetSummary()
-{
-    return $"{_date} {GetType().Name} ({_minutes} min): Distance {GetDistance():F1} miles, Speed {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
-}
+    public abstract double GetSpeed();
 
+    public abstract double GetPace();
+
+    // Shared summary method
+    public virtual string GetSummary()
+    {
+        return $"{_date} {GetType().Name} ({_minutes} min): Distance {GetDistance():F1} miles, Speed {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
+    }
 }
